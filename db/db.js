83d7e24acd0f1cc72ad992cca6beb489ca879dbf
izroth <lakhansharma889027@gmail.com>
2024-06-10 21:51:45 -0700

    chore: Add .env file to gitignore

---
+++ db/db.js
@@ -0,0 +1,17 @@
+const Mongoose = require("mongoose");
+require("dotenv").config();
+const url = process.env.DBURL;
+console.log(url,"url");
+const db = Mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.log("Could not connect to MongoDB...");
+    process.exit();
+  });
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
+require("./db/db");
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    });
-    
+  console.log(`Server is running on port ${port}`);
+});
